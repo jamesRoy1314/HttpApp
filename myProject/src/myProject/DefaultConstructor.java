cde008203c26162cee1e98197cfe2702ba0a4524
jamesRoy1314 <wangw5512@163.com>
2016-09-26 16:11:33 +0800

    second commit

---
+++ myProject/src/myProject/DefaultConstructor.java
@@ -0,0 +1,12 @@
+package myProject;
+class Brid{
+	int i;
+}
+public class DefaultConstructor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Brid nc = new Brid();
+	}
+
+}
